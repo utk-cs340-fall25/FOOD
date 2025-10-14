50a66bdf3f8a90303a017fc990c8e834bd9d7850
William <emolder@vols.utk.edu>
2025-10-14 08:19:36 -0400

    Removed dependencies on file_manager
    Modified the file input and output to be compatible with multithreading
    behavior.
    
    Added a fraction to double converter.

--- header_implements/food.cpp
+++ header_implements/food.cpp
@@ -7,8 +7,9 @@
 #include <iostream>
 #include <vector>
 #include <filesystem>
-
-namespace file = std::filesystem;
+#include <fstream>
+#include <sstream>
+#include <future>
 
 struct Ingredient
 {
@@ -26,17 +27,17 @@ struct Recipe
 
 std::map<int64_t, std::string> ERROR_MSGS;
 
-uint64_t ERROR_PRINTER(int64_t status, std::string prefix)
+void ERROR_PRINTER(int64_t status, std::string prefix)
 {
     std::cerr << prefix << ": ";
     if (ERROR_MSGS.find(status) == ERROR_MSGS.end())
     {
         std::cerr << "Invalid or undefined error code";
-        return 0;
+        return;
     }
 
     std::cerr << ERROR_MSGS[status];
-    return 0;
+    return;
 }
 void STATUS_LOADER()
 {
@@ -54,21 +55,36 @@ void INIT(std::list<Recipe> &saved_recipes)
 {
     STATUS_LOADER();
 
-    File_Manager loader;
-    std::list<Recipe> loaded_recipes;
-    uint64_t status;
+    std::vector <std::string> file_names;
+    std::vector<Recipe> loaded_recipes;
+    std::vector<std::future<uint64_t>> threads;
+    std::vector<uint64_t> statuses;
 
-    file::directory_iterator dir_it("../recipes");
-    for (dir_it; dir_it != std::filesystem::end(dir_it); dir_it++)
+    uint64_t file_num;
+    for(const std::filesystem::directory_entry &entry : std::filesystem::directory_iterator("../recipes"))
     {
-        struct Recipe recipe;
-        status = loader.Read_Recipe(((*dir_it).path()).u8string(), recipe);
-        if (status != STATUS_SUCCESS) continue;
-
-        loaded_recipes.push_back(recipe);
+        file_num += 1;
     }
 
-    saved_recipes = loaded_recipes;
+    loaded_recipes.resize(file_num);
+    file_names.resize(file_num);
+    threads.resize(file_num);
+    statuses.resize(file_num);
+
+    uint64_t i = 0;
+    for (const std::filesystem::directory_entry &entry : std::filesystem::directory_iterator{"../recipes"})
+    {
+        threads.at(i) = std::async(Read_Recipe, entry.path().u8string(), loaded_recipes.at(i));
+        file_names.at(i) = entry.path().u8string();
+        i += 1;
+    }
+
+    for (uint64_t i = 0; i < file_num; i += 1) { statuses.at(i) = threads.at(i).get(); }
+    for (uint64_t i = 0; i < file_num; i += 1)
+    {
+        if (statuses.at(i) != STATUS_SUCCESS) { ERROR_PRINTER(statuses.at(i), file_names.at(i));}
+    }
+
     return;
 }
 void DEINIT()
@@ -87,3 +103,128 @@ std::string to_lower(std::string string)
 
     return output;
 };
+STATUS fraction_to_double(std::string input_str, double &result)
+{
+    std::string numerator_str, denominator_str;
+    double numerator_d, denominator_d;
+    std::stringstream stream;
+    stream.str(input_str);
+
+    std::getline(stream, numerator_str, '/');
+    std::getline(stream, denominator_str);
+    stream.clear();
+
+    try
+    {
+        numerator_d = static_cast<double>(std::stoi(numerator_str));
+        denominator_d = static_cast<double>(std::stoi(denominator_str));
+    }
+    catch(std::invalid_argument) { return STATUS_BAD_ARGUMENTS; }
+
+    result = numerator_d;
+    result /= denominator_d;
+
+    return STATUS_SUCCESS;
+}
+
+/*
+** Read a file from the given path.
+*/
+STATUS Read_Recipe(const std::string path, struct Recipe& output_recipe)
+{
+    if (path.substr(path.length() - 4, 4) != ".rcp") return STATUS_BAD_EXTENSION;
+
+    std::ifstream file_input(path, std::ios::in);
+    if (!file_input.is_open())
+    {
+        if (!std::filesystem::exists(path)) return STATUS_FILE_NOT_FOUND;
+        else return STATUS_OPEN_FAILED;
+    }
+
+    // File Reading
+    struct Recipe recipe;
+    std::string temp_string;
+
+    file_input >> recipe.name;
+    recipe.name = to_lower(recipe.name);
+    temp_string.resize(1);
+
+    while (to_lower(temp_string) != "i")
+    {
+        std::string::size_type sz;
+        struct Ingredient temp_ingredient;
+        file_input >> temp_ingredient.amount_s >> temp_ingredient.unit >> temp_ingredient.name;
+        temp_ingredient.name = to_lower(temp_ingredient.name);
+        try
+        {
+            temp_ingredient.amount_d = fraction_to_double(temp_ingredient.amount_s);
+        }
+        catch (std::invalid_argument) { return STATUS_INVALID_DATA; }
+
+        recipe.ingredients.push_back(temp_ingredient);
+        
+        char temp_char = file_input.peek();
+        while (isspace((int)temp_char))
+        {
+            file_input >> std::noskipws >> temp_char;
+            temp_char = file_input.peek();
+        }
+        
+        temp_string.at(0) = temp_char;
+    }
+
+    std::getline(file_input, temp_string, '\n');
+    while (std::getline(file_input, temp_string, '\n'))
+    {
+        if (temp_string.length() == 0) continue;
+        recipe.instructions.push_back(temp_string);
+    }
+
+    output_recipe = recipe;
+    file_input.close();
+    return STATUS_SUCCESS;
+}
+STATUS Write_Recipe(const std::string path, const struct Recipe& output_recipe, bool overwrite)
+{
+    if (path.substr(path.length() - 4, 4) != ".rcp") return STATUS_BAD_EXTENSION;
+    if (std::filesystem::exists(path) && !overwrite) return STATUS_FILE_EXISTS;
+
+    std::ofstream file_output(path, std::ios::trunc | std::ios::out);
+    if (!file_output.is_open())
+    {
+        if (!std::filesystem::exists(path)) return STATUS_FILE_NOT_FOUND;
+        else return STATUS_OPEN_FAILED;
+    }
+    if (output_recipe.name == "") return STATUS_INVALID_DATA;
+
+    std::list<std::string> output_buffer;
+    output_buffer.push_back(to_lower(output_recipe.name) + "\n\n");
+
+    for (uint64_t i = 0; i < output_recipe.ingredients.size(); i += 1)
+    {
+        if (output_recipe.ingredients.at(i).amount_s == "") return STATUS_INVALID_DATA;
+        if (output_recipe.ingredients.at(i).unit == "") return STATUS_INVALID_DATA;
+
+        output_buffer.push_back(output_recipe.ingredients.at(i).amount_s + ' ' +
+            output_recipe.ingredients.at(i).unit + ' ' +
+            output_recipe.ingredients.at(i).name + '\n');
+    }
+
+    output_buffer.push_back("\n");
+    output_buffer.push_back("instructions\n");
+
+    for (uint64_t i = 0; i < output_recipe.instructions.size(); i += 1)
+    {
+        if (output_recipe.instructions.at(i) == "") return STATUS_INVALID_DATA;
+        output_buffer.push_back(output_recipe.instructions.at(i) + '\n');
+    }
+
+    while (output_buffer.size() > 0)
+    {
+        file_output << output_buffer.front();
+        output_buffer.pop_front();
+    }
+
+    file_output.close();
+    return STATUS_SUCCESS;
+}
--- headers/food.hpp
+++ headers/food.hpp
@@ -4,7 +4,8 @@
 #include <string>
 #include <list>
 
-enum STATUS
+// Status Codes
+enum STATUS : uint64_t
 {
     STATUS_SUCCESS, // Success Status
     STATUS_OPEN_FAILED,
@@ -15,13 +16,19 @@ enum STATUS
     STATUS_BAD_EXTENSION
 };
 
-struct Ingredient;
-struct Recipe;
+typedef uint64_t STATUS;
 
-void STATUS_LOADER();
-uint64_t ERROR_PRINTER(int64_t status, std::string prefix);
-void INIT(std::list<Recipe>&);
+struct Ingredient; // Structure for an ingredient
+struct Recipe; // Structure for a recipe
+
+void STATUS_LOADER(); // Loads status messages
+void ERROR_PRINTER(int64_t status, std::string prefix); // Prints status messages
+void INIT(std::list<Recipe>&); // Initializes basic data for the app
 void DEINIT();
 
-std::string to_lower(std::string);
+std::string to_lower(std::string); // Converts a string to all lowercase
+STATUS fraction_to_double(std::string, double&); // Converts a string-fraction into a double
+
+STATUS Read_Recipe(const std::string, struct Recipe&); // This will read a .rcp file
+STATUS Write_Recipe(const std::string, const struct Recipe&, bool); // This will write to a .rcp file
 
